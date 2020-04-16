ada617de254844630fc8a7bed5bcebb2c4d1a024
gekal <liu.hongying@hotmail.com>
2020-04-16 23:13:45 +0900

    init commit

---
+++ bin/cdk-api-sample.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkApiSampleStack } from '../lib/cdk-api-sample-stack';
+
+const app = new cdk.App();
+new CdkApiSampleStack(app, 'CdkApiSampleStack');
